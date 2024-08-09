57f1b50f38ac5e993d33a97b0cb294857bc9cf97
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2024-08-09 22:41:21 +0530

    first commit

---
+++ getElementById/index.js
@@ -0,0 +1,10 @@
+//printing in console 
+console.log (`HelloWorld`);
+
+//window alert
+window.alert (`This is an alert`);
+
+//getElementById
+
+document.getElementById ("h1").textContent = `Hello`;
+document.getElementById ("myP").textContent = `this is a paragraph`;
