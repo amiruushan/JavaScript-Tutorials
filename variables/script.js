7851bab6cdf37bf8e2ca506259a782e27f88a128
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2024-08-11 20:09:35 +0530

    variables update

---
+++ variables/script.js
@@ -0,0 +1,29 @@
+// declaration
+let z;
+z = 2+2;
+console.log (z);
+
+// asignment
+y = 10;
+console.log (y);
+
+// declaration and assignment
+let x = 100;
+console.log (x);
+
+// using variables
+let age = 19;
+console.log(`I am ${age} years old`);
+
+let name = "Amiru Ushan";
+console.log(`My name is ${name}`);
+
+// printing the type of a variable
+console.log(typeof age);
+console.log(typeof name);
+
+// activity
+console.log(`________________`);
+let num = 2;
+let type = typeof num;
+console.log(`${num} is a ${type}`);
