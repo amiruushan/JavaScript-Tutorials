0b06e01ba9e369c99e76dd43d1bac80addad0cf8
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2024-08-24 23:08:52 +0530

    delete

--- 08 Random Numbers/script.js
+++
@@ -1,8 +0,0 @@
-const min = 5;
-const max = 7;
-
-// generating a randome number between min and max
-
-let randomNumber = Math.trunc(Math.random() * (max - min + 1)) + min ;
-
-document.getElementById("myH3").textContent = (`Random number between ${min} and ${max} - ${randomNumber}`);
