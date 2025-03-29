1c47f7e460d794aeac855622fa9f425ca4bb949b
Amiru Ushan <152049636+amiruushan@users.noreply.github.com>
2025-03-29 10:00:55 +0530

    rest parameters

--- 24 spread operator/script.js
+++ 24 spread operator/script.js
@@ -12,7 +12,7 @@ const food = [...fruits, ...vegetables];
 console.log(food);
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-let max = Math.max(numbers)
+let max = Math.max(...numbers)
 let min = Math.min(...numbers)
 
 console.log("02 - get elements from an array");
